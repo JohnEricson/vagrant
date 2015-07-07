JohnEricson/vagrant

commit 1c884fa4e5a847549afa45350e37b0362c50feae

provisioners/ansible: Show Ansible command when VAGRANT_LOG=debug

plugins/provisioners/ansible/provisioner.rb
@@ -66,7 +66,7 @@ module VagrantPlugins
         # Support Multiple SSH keys and SSH forwarding:
         env["ANSIBLE_SSH_ARGS"] = ansible_ssh_args unless ansible_ssh_args.empty?
 
-        show_ansible_playbook_command(env, command) if config.verbose
+        show_ansible_playbook_command(env, command) if (config.verbose || @logger.debug?)
 
         # Write stdout and stderr data, since it's the regular Ansible output
         command << {

test/unit/plugins/provisioners/ansible/provisioner_test.rb
@@ -50,6 +50,8 @@ VF
   let(:generated_inventory_file) { File.join(generated_inventory_dir, 'vagrant_ansible_inventory') }
 
   before do
+    subject.instance_variable_get(:@logger).stub(:debug?).and_return(false)
+
     machine.stub(ssh_info: ssh_info)
     machine.env.stub(active_machines: [[iso_env.machine_names[0], :dummy], [iso_env.machine_names[1], :dummy]])
 
@@ -461,6 +463,19 @@ VF
         expect(Vagrant::Util::Subprocess).to receive(:execute).with { |*args|
           cmd_opts = args.last
           expect(cmd_opts[:env]['ANSIBLE_SSH_ARGS']).to include("-o ForwardAgent=yes")
+        }
+      end
+    end
+
+    describe "with VAGRANT_LOG=debug, but without verbose option" do
+      before do
+        subject.instance_variable_get(:@logger).stub(:debug?).and_return(true)
+        config.verbose = false
+      end
+
+      it "shows the ansible-playbook command" do
+        expect(machine.env.ui).to receive(:detail).with { |full_command|
+          expect(full_command).to eq("ANSIBLE_FORCE_COLOR=true ANSIBLE_HOST_KEY_CHECKING=false PYTHONUNBUFFERED=1 ansible-playbook --private-key=/path/to/my/key --user=testuser --limit='machine1' --inventory-file=#{generated_inventory_dir} playbook.yml")
         }
       end
     end

commit 48d38b0f1f0a40a4bf3c88c6771e2a3be77cbdf3

provisioners/ansible: show ansible-playbook only when the verbose option is enabled
Revert 1c884fa4e5a847549afa45350e37b0362c50feae which introduced the
following bug:

Instead of allowing to dump the `ansible-playbook` command details when
VAGRANT_LOG=debug was defined, it was then impossible to disable this
console output when VAGRANT_LOG was undefined (in such case,
``@logger.debug? systematically returns `true`)

In order to keep things simple and focused, it is preferable to drop the
bad idea to mix Ansible verbosity and Vagrant log level.

Fix #5803

plugins/provisioners/ansible/provisioner.rb
@@ -81,7 +81,7 @@ module VagrantPlugins
         # ANSIBLE_SSH_ARGS is required for Multiple SSH keys, SSH forwarding and custom SSH settings
         env["ANSIBLE_SSH_ARGS"] = ansible_ssh_args unless ansible_ssh_args.empty?
 
-        show_ansible_playbook_command(env, command) if (config.verbose || @logger.debug?)
+        show_ansible_playbook_command(env, command) if config.verbose
 
         # Write stdout and stderr data, since it's the regular Ansible output
         command << {

test/unit/plugins/provisioners/ansible/provisioner_test.rb
@@ -51,7 +51,6 @@ VF
 
   before do
     Vagrant::Util::Platform.stub(solaris?: false)
-    subject.instance_variable_get(:@logger).stub(:debug?).and_return(false)
 
     machine.stub(ssh_info: ssh_info)
     machine.env.stub(active_machines: [[iso_env.machine_names[0], :dummy], [iso_env.machine_names[1], :dummy]])
@@ -461,19 +460,6 @@ VF
         expect(Vagrant::Util::Subprocess).to receive(:execute).with { |*args|
           cmd_opts = args.last
           expect(cmd_opts[:env]['ANSIBLE_SSH_ARGS']).to include("-o ForwardAgent=yes")
-        }
-      end
-    end
-
-    describe "with VAGRANT_LOG=debug, but without verbose option" do
-      before do
-        subject.instance_variable_get(:@logger).stub(:debug?).and_return(true)
-        config.verbose = false
-      end
-
-      it "shows the ansible-playbook command" do
-        expect(machine.env.ui).to receive(:detail).with { |full_command|
-          expect(full_command).to eq("PYTHONUNBUFFERED=1 ANSIBLE_HOST_KEY_CHECKING=false ANSIBLE_FORCE_COLOR=true ANSIBLE_SSH_ARGS='-o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes -o ControlMaster=auto -o ControlPersist=60s' ansible-playbook --private-key=/path/to/my/key --user=testuser --connection=ssh --timeout=30 --limit='machine1' --inventory-file=#{generated_inventory_dir} playbook.yml")
         }
       end
     end